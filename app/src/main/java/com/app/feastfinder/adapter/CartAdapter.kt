df9dfd0d1f042fe64a8ef7b63176be6e426646d5
Dell <syeduzair99jamal@gmail.com>
2024-04-26 02:06:28 +0500

    done with detailActivity till cartItems added halfway.

--- app/src/main/java/com/app/feastfinder/adapter/CartAdapter.kt
+++ app/src/main/java/com/app/feastfinder/adapter/CartAdapter.kt
@@ -1,13 +1,44 @@
 package com.app.feastfinder.adapter
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.feastfinder.databinding.CartItemBinding
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class CartAdapter(private val cartItems: MutableList<String>, private val cartItemImages: MutableList<Int>,private val cartItemPrice: MutableList<String>) : RecyclerView.Adapter<CartAdapter.CartViewHolder>()  {
+class CartAdapter(
+    private val context: Context,
+    private val cartItems: MutableList<String>,
+    private val cartItemImages: MutableList<String>,
+    private val cartItemPrice: MutableList<String>,
+    private var cartDescription: MutableList<String>,
+    private val cartQuantity: MutableList<Int>
 
-    private val itemQuantities = IntArray(cartItems.size){1}
+
+    ) : RecyclerView.Adapter<CartAdapter.CartViewHolder>()  {
+
+    private val auth = FirebaseAuth.getInstance()
+
+    init{
+        val database = FirebaseDatabase.getInstance()
+        val userId = auth.currentUser?.uid?:""
+        val cartItemNumber = cartItems.size
+
+        itemQuantities = IntArray(cartItemNumber){1}
+        cartItemsReference = database.reference.child("user").child(userId).child("cartItems")
+    }
+    companion object{
+        private var itemQuantities: IntArray = intArrayOf()
+        private lateinit var cartItemsReference: DatabaseReference
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartAdapter.CartViewHolder {
         return CartViewHolder(CartItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
@@ -25,9 +56,14 @@ class CartAdapter(private val cartItems: MutableList<String>, private val cartIt
     }
 
     inner class CartViewHolder(private val binding : CartItemBinding ): RecyclerView.ViewHolder(binding.root) {
-        fun bind(cartItem: String, cartImages: Int, cartPrice: String) {
+        fun bind(cartItem: String, cartImages: String, cartPrice: String) {
             binding.itemCartName.text = cartItem
-            binding.cartImg.setImageResource(cartImages)
+            val uriString: String = cartImages
+            val uri: Uri = Uri.parse(uriString)
+
+            // Load images
+            Glide.with(context).load(uri).into(binding.cartImg)
+
             binding.cartItemPrice.text = cartPrice
             val quantity = itemQuantities[position]
             binding.cartQuantity.text = quantity.toString()
@@ -45,14 +81,30 @@ class CartAdapter(private val cartItems: MutableList<String>, private val cartIt
                 }
             }
             binding.deleteCart.setOnClickListener {
-                cartItems.removeAt(position)
-                cartItemImages.removeAt(position)
-                cartItemPrice.removeAt(position)
-                notifyItemRemoved(position)
-                notifyItemRangeChanged(position,cartItems.size)
+                val positionRetrieve = position
+                getUniqueKeyAtPosition(positionRetrieve)
+
             }
+        }
+        private fun getUniqueKeyAtPosition(positionRetrieve: Int) {
+            cartItemsReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var uniqueKey:String? = null
 
+                    snapshot.children.forEachIndexed { index, dataSnapshot ->
+                        if(index == positionRetrieve){
+                            uniqueKey = dataSnapshot.key
+                            return@forEachIndexed
+                        }
 
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
         }
 
     }
